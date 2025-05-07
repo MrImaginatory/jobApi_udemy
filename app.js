beef687c4e0380c9f2e63207b8b6c998e00f3c0f
Illusionary <illusionary@duck.com>
2025-05-07 13:06:18 +0530

    Added some Security Packages

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import cors from 'cors';
+import xss from 'xss-clean';
+import rateLimiter from 'express-rate-limit';
 
+// routesImport
 import authRouter from './routes/auth.route.js';
 import jobRouter from './routes/job.route.js';
 
-
 const app = express();
 
+app.set('trust proxy',1);
+app.use(rateLimiter({
+    windowMs:15*60*1000,
+    max:100,
+}))
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 app.use(cookieParser());
 
 // Routes
